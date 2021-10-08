8225b83eb8b0c85b135f8a6e255abafff0e7c39b
Hyoin-kim <hi980506@naver.com>
2021-10-09 02:42:05 +0900

    feat : 기본 세팅 구현

---
+++ about-me/src/App.js
@@ -0,0 +1,14 @@
+import {Header,Footer,AboutMe} from './component';
+
+
+function App() {
+  return (
+      <div>
+        <Header/>
+        <AboutMe/>
+        <Footer/>  
+      </div>
+  );
+}
+
+export default App;
---
+++ about-me/src/assets/image/index.js
@@ -0,0 +1,5 @@
+export { default as github } from './github.png';
+export { default as facebook } from './facebook.png';
+export { default as instagram } from './instagram.png';
+export { default as gmail } from './gmail.png';
+export { default as backgroundImg } from './marnie.jpg';
