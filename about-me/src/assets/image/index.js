91035fe361d59ea3f20349220555b739c544e2c2
Hyoin-kim <hi980506@naver.com>
2021-10-10 22:00:56 +0900

    assets : 이미지 추가 및 index정리

--- about-me/src/assets/image/index.js
+++ about-me/src/assets/image/index.js
@@ -3,3 +3,5 @@ export { default as facebook } from './facebook.png';
 export { default as instagram } from './instagram.png';
 export { default as gmail } from './gmail.png';
 export { default as backgroundImg } from './marnie.jpg';
+export { default as user } from './user.png';
+export { default as skills } from './skill.png';
