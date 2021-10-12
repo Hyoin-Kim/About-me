6374418242d41256b604d64a996a48e5aa485183
Hyoin-kim <hi980506@naver.com>
2021-10-12 15:52:47 +0900

    etc : index.js 폴더 경로 정리

--- about-me/src/component/index.js
+++ about-me/src/component/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './common/Header';
 export { default as Footer } from './common/Footer';
 export { default as AboutMe } from './common/AboutMe';
+export { default as Skills } from './common/Skills';
+export { default as Project } from './common/Project';
