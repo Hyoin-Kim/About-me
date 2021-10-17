17ca8a59b73c6db2050c50561bfdf92e9866b581
Hyoin-kim <hi980506@naver.com>
2021-10-17 14:34:28 +0900

    etx : 파일 export 정리

--- about-me/src/component/index.js
+++ about-me/src/component/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './common/Footer';
 export { default as AboutMe } from './common/AboutMe';
 export { default as Skills } from './common/Skills';
 export { default as Project } from './common/Project';
+export { default as Activity } from './common/Activity';
