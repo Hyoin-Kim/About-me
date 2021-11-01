6ee012852a3893f1ba7dd0519a3d4b92da287a72
Hyoin-kim <hi980506@naver.com>
2021-11-01 22:48:15 +0900

    Feat : Activity 일부 구현

--- about-me/src/component/common/Activity.js
+++ about-me/src/component/common/Activity.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import {HyoinProfile} from '../../assets/image/index';
 
 const ActivityWrapper = styled.div`
 
@@ -33,6 +34,45 @@ const ActivityWrapper = styled.div`
             font-variation-settings: "wght" 100, "ital" 0;
             text-shadow: none;
         }
+
+        &__detail{
+            font-family: 'Noto Sans KR', sans-serif;
+            align-items :center;
+            margin: 10px;
+            display : inline-block;
+        }
+
+        &__main{
+            display : inline-block;
+            justify-content: center;
+            flex-wrap: wrap;
+            list-style-type: none;
+            padding: 0;
+        }
+
+        &__date{
+            width : 140px;
+            margin-left : 40px;
+            margin-right : 20px;
+
+            &-ing{
+                width : 140px;
+                margin-left : 40px;
+                margin-right : 20px;
+                padding-right : 38px;
+            }
+        }
+
+        &__profile{
+            width : 400px;
+            height : 400px;
+            display : inline-block;
+            margin: 0px 30px 30px 50px;
+        }
+
+        &__content{
+            margin :10px;
+        }
     }
 `
 
@@ -45,16 +85,19 @@ const Activity = () => {
                         <h1 className="skills__title">ACTIVITIES</h1>
                     </div>
                 </div>
-                <div>
-                    <div>2017.03 - 서울여자대학교 자율전공학부 입학</div>
-                    <div>2018.07 - 2018.08 Programming Guru1 수료 (HTML, PYTHON, UNITY 단기 프로젝트 진행)</div>
-                    <div>2018.08 - 2019.12 서울여자대학교 정보보호학과 리눅스 소학회 SWLUG 활동</div>
-                    <div>2019.01 - 2019.02 Programming Guru2 수료 (ANDROID 프로젝트 "질문있슈" 진행)</div>
-                    <div>2019.07 - 2019.11 네트워크 응용분야 NET Challenge 공모전(은상 수상)</div>
-                    <div>2020.07 - 2020.08 예쉬컴퍼니 VR 기획 및 개발 인턴 수료 </div>
-                    <div>2021.03 - 2021.07 IT 연합 창업동아리 SOPT 28기 Web 수료</div>
-                    <div>2021.09 - IT 연합 창업동아리 SOPT 29기 Web 활동 중</div>
-                </div>
+                <nav classname="skills__main">
+                    <img className="skills__profile" src={HyoinProfile} alt="" />
+                    <div className="skills__detail">
+                        <div className="skills__content"><span className="skills__date">2017.03 - 2022.02</span>서울여자대학교 정보보호학과 입학 및 졸업 예정</div>
+                        <div className="skills__content"><span className="skills__date">2018.07 - 2018.08 </span>Programming Guru1 수료 (HTML, PYTHON, UNITY 단기 프로젝트 진행)</div>
+                        <div className="skills__content"><span className="skills__date">2018.08 - 2019.12 </span>서울여자대학교 정보보호학과 리눅스 소학회 SWLUG 활동</div>
+                        <div className="skills__content"><span className="skills__date">2019.01 - 2019.02 </span>Programming Guru2 수료 (ANDROID 프로젝트 "질문있슈" 진행)</div>
+                        <div className="skills__content"><span className="skills__date">2019.07 - 2019.11 </span>네트워크 응용분야 NET Challenge 공모전(은상 수상)</div>
+                        <div className="skills__content"><span className="skills__date">2020.07 - 2020.08 </span>예쉬컴퍼니 VR 기획 및 개발 인턴 수료 </div>
+                        <div className="skills__content"><span className="skills__date">2021.03 - 2021.07 </span>IT 연합 창업동아리 SOPT 28기 Web 수료</div>
+                        <div className="skills__content"><span className="skills__date-ing">2021.09 - ing</span>IT 연합 창업동아리 SOPT 29기 Web 활동 중</div>
+                    </div>
+                </nav>
                 
             </ActivityWrapper>
         </div>
