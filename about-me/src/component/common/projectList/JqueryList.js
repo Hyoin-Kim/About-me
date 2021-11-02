2e556d7fb97ab68ab45c0f316134422f844b5306
Hyoin-kim <hi980506@naver.com>
2021-11-03 02:10:36 +0900

    feat : jQuery 프로젝트 관련 일부 구현

--- about-me/src/component/common/projectList/JqueryList.js
+++ about-me/src/component/common/projectList/JqueryList.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+import { kikiImg } from '../../../assets/image';
+
+const JqueryWrapper = styled.div`
+    .jquery{
+        font-family: 'Noto Sans KR', sans-serif;
+        display : flex;
+        justify-content : center;
+
+        &__main{
+            font-family: 'Noto Sans KR', sans-serif;
+            display :flex;
+            margin : 1px;
+            justify-content : center;
+            cursor : pointer;
+            border: 1px solid;
+            width : 300px;
+            height : 300px;
+
+        }
+        &__img{
+            transition: all 0.5s;
+            opacity : 0.5;
+        }
+        &__img:hover{
+            transition: all 0.5s;
+            opacity: 1;
+        }
+        &__title{
+            font-weight : bold;
+            position:absolute;
+            z-index : 1;
+        }
+
+    }
+`;
 
 const JqueryList = () => {
     return (
-        <div>
-            
-        </div>
+        <JqueryWrapper>
+                <div className="jquery">
+                    <div className="jquery__main">
+                        <img className="jquery__img"src={kikiImg} alt=""/>
+                        <span className="jquery__title">Ghibli Blog</span>
+                    </div>
+                </div>
+        </JqueryWrapper>
     );
 };
 
