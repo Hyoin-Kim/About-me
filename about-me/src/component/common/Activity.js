c0ea895177b449d9f4276a3bbcbe70471d385467
Hyoin-kim <hi980506@naver.com>
2021-11-03 02:09:25 +0900

    feat : activity 카테고리 부분 푸터 추가 및 스타일 변경

--- about-me/src/component/common/Activity.js
+++ about-me/src/component/common/Activity.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {HyoinProfile} from '../../assets/image/index';
+import {HyoinProfile, society} from '../../assets/image/index';
 
 const ActivityWrapper = styled.div`
-
+    text-align: -webkit-center;
     .skills{
         display : flex;
         justify-content : center;
 
         &__img{
             margin-right : 10px;
-            width : 40px;
-            height : 40px;
+            width : 50px;
+            height : 50px;
             display : flex;
             margin-top :10px;
         }
@@ -27,7 +27,8 @@ const ActivityWrapper = styled.div`
             text-shadow: 10px 10px 0px #d6f4f4,
             15px 15px 0px #F5A9F2,
             20px 20px 0px #CC2EFA,
-            cursor: pointer
+            cursor: pointer;
+            margin-bottom : 20px;
         }
 
         &__title:hover {
@@ -40,6 +41,8 @@ const ActivityWrapper = styled.div`
             align-items :center;
             margin: 10px;
             display : inline-block;
+            vertical-align : middle;
+            text-align : start;
         }
 
         &__main{
@@ -71,7 +74,25 @@ const ActivityWrapper = styled.div`
         }
 
         &__content{
-            margin :10px;
+            margin : 10px;
+        }
+
+        &__footer{
+            margin : 30px 0px 10px 45px;
+            font-size : 22px;
+            font-family: 'Noto Sans KR', sans-serif;
+            font-weight : bold;
+        }
+
+        &__name{
+            cursor : pointer;
+            transition: all 0.5s;
+        }
+
+        &__name:hover{
+            color : #eb285d;
+            transition: all 0.5s;
+            font-size : 25px;
         }
     }
 `
@@ -82,6 +103,7 @@ const Activity = () => {
             <ActivityWrapper>
                 <div>
                     <div className="skills">
+                        <img className="skills__img" src={society} alt=""/>
                         <h1 className="skills__title">ACTIVITIES</h1>
                     </div>
                 </div>
@@ -96,6 +118,10 @@ const Activity = () => {
                         <div className="skills__content"><span className="skills__date">2020.07 - 2020.08 </span>예쉬컴퍼니 VR 기획 및 개발 인턴 수료 </div>
                         <div className="skills__content"><span className="skills__date">2021.03 - 2021.07 </span>IT 연합 창업동아리 SOPT 28기 Web 수료</div>
                         <div className="skills__content"><span className="skills__date-ing">2021.09 - ing</span>IT 연합 창업동아리 SOPT 29기 Web 활동 중</div>
+                        <div className="skills__footer">
+                            <div className="skills__footer-detail"> 스스로 문제를 찾아 고민하고, 정의하고, 해결할 수 있는 능력을 갖춘,</div>
+                            <div className="skills__footer-detail"> 서비스 플랫폼 개발자 <span className="skills__name">“김효인”</span> 이 되겠습니다.</div>
+                        </div>
                     </div>
                 </nav>
                 
