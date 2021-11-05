dfec5d49f74dc0e2e1f11238a32b82c87e2a0c02
Hyoin-kim <hi980506@naver.com>
2021-11-05 14:03:36 +0900

    etc : 필요한 이미지 export 정리

--- about-me/src/assets/image/index.js
+++ about-me/src/assets/image/index.js
@@ -2,6 +2,7 @@ export { default as github } from './github.png';
 export { default as facebook } from './facebook.png';
 export { default as instagram } from './instagram.png';
 export { default as gmail } from './gmail.png';
+export { default as blog } from './blog.png';
 export { default as backgroundImg } from './marnie.jpg';
 export { default as user } from './user.png';
 export { default as skills } from './skill.png';
--- about-me/src/component/index.js
+++ about-me/src/component/index.js
@@ -4,3 +4,7 @@ export { default as AboutMe } from './common/AboutMe';
 export { default as Skills } from './common/Skills';
 export { default as Project } from './common/Project';
 export { default as Activity } from './common/Activity';
+export { default as JavascriptList } from './common/projectList/JavascriptList';
+export { default as MachineLearning } from './common/projectList/MachineLearning';
+export { default as JqueryList } from './common/projectList/JqueryList';
+export { default as ReactList} from './common/projectList/ReactList';
