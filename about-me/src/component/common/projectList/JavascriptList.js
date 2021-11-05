5f46ace9af3aad9fe5e77149a103cb8d18c995de
Hyoin-kim <hi980506@naver.com>
2021-11-05 14:04:45 +0900

    refactor : 스타일 수정 및 불필요한 코드 정리

--- about-me/src/component/common/projectList/JavascriptList.js
+++ about-me/src/component/common/projectList/JavascriptList.js
@@ -5,8 +5,8 @@ import { kikiImg } from '../../../assets/image';
 import {closeIcon, js, jsSlider,jsSlider2,jsSlider3,jsSlider4,jsSlider5} from '../../../assets/image/index';
 
 
-const JqueryWrapper = styled.div`
-    .jquery{
+const JsWrapper = styled.div`
+    .js{
         font-family: 'Noto Sans KR', sans-serif;
         display : flex;
         justify-content : center;
@@ -14,10 +14,9 @@ const JqueryWrapper = styled.div`
         &__main{
             font-family: 'Noto Sans KR', sans-serif;
             display :flex;
-            margin : 1px;
+            margin : 10px;
             justify-content : center;
             cursor : pointer;
-            border: 1px solid;
             width : 300px;
             height : 300px;
 
@@ -25,6 +24,7 @@ const JqueryWrapper = styled.div`
         &__img{
             transition: all 0.5s;
             opacity : 0.5;
+            border-radius : 20px;
         }
         &__img:hover{
             transition: all 0.5s;
@@ -33,9 +33,7 @@ const JqueryWrapper = styled.div`
         &__title{
             font-weight : bold;
             position:absolute;
-            z-index : 1;
         }
-
     }
 `;
 
@@ -49,10 +47,10 @@ const ModalWrapper = styled.div`
         border-width: 1px;
         border-style: solid;
         border-radius: 16px;
-        border-color:black;
         background-color: white;
         width: 700px;
         height: 800px;
+        z-index :1;
 
         .closeIcon{
             width:20px;
@@ -85,39 +83,48 @@ const ModalWrapper = styled.div`
                 font-size : 17px;
                 margin: 10px 0px 0px 5px;
             }
+            &__jsTitle{
+                font-size : 20px;
+                font-weight : bold;
+            }
         }
 
         p{
             font-family: 'Noto Sans KR', sans-serif;
             margin-left : 10px;
             margin-right : 10px;
+            text-align : start;
+        }
+        span{
+            color : #eb285d;
+        }
+
+        .text{
+            margin : 10px;
         }
 `;
 
 const Modal = styled.div`
-        position: fixed;
-        z-index : 100;
-          position : fixed;
-          top : 0;
-          left : 0;
-          overflow-y: scroll; 
-          width: 100%;
+    position: fixed;
+    top:0; left: 0; bottom: 0; right: 0;
+    background: rgba(0, 0, 0, 0.8);
+    z-index :1;
 
 `;
 const JavascriptList = () => {
 
     const [ghibliModal,setGhibliModal] = useState(false);
-    function jqueryHandleModal(){
+    function jsHandleModal(){
         setGhibliModal(true);
 
     }
 
     return (
-            <JqueryWrapper>
-                    <div className="jquery">
-                        <div className="jquery__main">
-                            <img className="jquery__img" src={js} alt="" onClick={()=>{jqueryHandleModal()}}/>
-                            <span className="jquery__title">Vanilla JS Project</span>
+            <JsWrapper>
+                    <div className="js">
+                        <div className="js__main">
+                            <img className="js__img" src={js} alt="" onClick={()=>{jsHandleModal()}}/>
+                            <span className="js__title">Vanilla JS Project</span>
                         </div>
                     </div>
 
@@ -143,23 +150,25 @@ const JavascriptList = () => {
                                     <img className="sop__slider"src={jsSlider3} alt=""/>
                                     </Carousel.Item>
                                     </Carousel>
-                                    <div>
+                                    <div className="text">
                                         <h2 className="modal__title">Vanilla JS 기반 웹 프로젝트</h2>
                                         <h5 className="modal__subtitle">Weather App, Paint App, To do List, Velog Clone, Smartphone-emulation</h5>
                                         <hr className="modal__hr"/>
-                                        <p>순수 자바스크립트 하나로 개발한 토이 프로젝트 입니다.</p>
+                                        <p><span className="modal__jsTitle">순수 자바스크립트 하나로 개발한 토이 프로젝트</span> 입니다.</p>
                                         <p>Weather App(#1), Paint App(#2), To do List(#3), Velog Clone(#4), Smartphone-emulation(#5) </p>
                                         <p>등 로컬스토리지를 활용하여 개발을 진행하였습니다.</p>
                                         <p>Smartphone-emulation(#5)은 알람(추가/삭제), 메모(추가/삭제/상세보기), 갤러리 기능을 구현하였습니다.</p>
                                         <p>이외의 토이 프로젝트 기능은 Github을 통해 확인 가능합니다.</p>
                                     </div>
+                                    <div>
                                     <img className="closeIcon" src={closeIcon} alt="" onClick={()=>{setGhibliModal(false)}}/>
+                                    </div>
                                 </ModalWrapper>
                             </Modal>
                             :
                             null
                         }
-            </JqueryWrapper>
+            </JsWrapper>
         );
     };
 
