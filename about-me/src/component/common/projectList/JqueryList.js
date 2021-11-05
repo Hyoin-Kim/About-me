106817cd5a08549c4c18ad144db269bef234cf3f
Hyoin-kim <hi980506@naver.com>
2021-11-05 14:04:51 +0900

    refactor : 스타일 수정 및 불필요한 코드 정리

--- about-me/src/component/common/projectList/JqueryList.js
+++ about-me/src/component/common/projectList/JqueryList.js
@@ -14,10 +14,9 @@ const JqueryWrapper = styled.div`
         &__main{
             font-family: 'Noto Sans KR', sans-serif;
             display :flex;
-            margin : 1px;
+            margin : 10px;
             justify-content : center;
             cursor : pointer;
-            border: 1px solid;
             width : 300px;
             height : 300px;
 
@@ -25,6 +24,7 @@ const JqueryWrapper = styled.div`
         &__img{
             transition: all 0.5s;
             opacity : 0.5;
+            border-radius : 20px;
         }
         &__img:hover{
             transition: all 0.5s;
@@ -33,7 +33,6 @@ const JqueryWrapper = styled.div`
         &__title{
             font-weight : bold;
             position:absolute;
-            z-index : 1;
         }
 
     }
@@ -49,15 +48,16 @@ const ModalWrapper = styled.div`
         border-width: 1px;
         border-style: solid;
         border-radius: 16px;
-        border-color:black;
         background-color: white;
         width: 700px;
         height: 800px;
+        z-index :1;
 
         .closeIcon{
             width:20px;
-            float:right;
-            margin : 10px;
+            float : right;
+            margin-bottom : 20px;
+            margin-right : 10px;
             z-index :1;
             cursor :pointer;
         }
@@ -85,23 +85,32 @@ const ModalWrapper = styled.div`
                 font-size : 17px;
                 margin: 10px 0px 0px 5px;
             }
+            &__jsTitle{
+                font-size : 20px;
+                font-weight : bold;
+            }
         }
 
         p{
             font-family: 'Noto Sans KR', sans-serif;
             margin-left : 10px;
             margin-right : 10px;
+            text-align : start;
+        }
+        span{
+            color : #eb285d;
+        }
+
+        .text{
+            margin:10px;
         }
 `;
 
 const Modal = styled.div`
-        position: fixed;
-        z-index : 100;
-          position : fixed;
-          top : 0;
-          left : 0;
-          overflow-y: scroll; 
-          width: 100%;
+    position: fixed;
+    top:0; left: 0; bottom: 0; right: 0;
+    background: rgba(0, 0, 0, 0.8);
+    z-index :1;
 
 `;
 
@@ -142,14 +151,15 @@ const JqueryList = () => {
                                 <img className="sop__slider"src={kikiSlider5} alt=""/>
                                 </Carousel.Item>
                                 </Carousel>
-                                <div>
+                                <div className="text">
                                     <h2 className="modal__title">친구들과 소통하고 공유할수 있는 Ghibli Blog</h2>
-                                    <h5 className="modal__subtitle">Studio Ghibli 테마 기반의 게시판, 앨범 등 타인과 소통가능한 웹서비스 입니다.</h5>
+                                    <h5 className="modal__subtitle">Studio Ghibli 테마 기반의 웹서비스</h5>
                                     <hr className="modal__hr"/>
+                                    <p>Studio Ghibli 테마 기반의 게시판, 앨범 등 <span className="modal__jsTitle">타인과 소통하고 공유하는 웹서비스</span> 입니다.</p>
                                     <p>프론트단에서 JQuery를 활용한 Javascript, 백엔드의 laravel 프레임워크를 활용한 php언어로 개발하였습니다.</p>
                                     <p>앨범(#2), 게시판(#3), 친구추가(#4), 메세지함(#5), 게시물 좋아요/싫어요, 댓글 등의 기능 구현하였습니다.</p>
-                                    <p>전체적인 웹 페이지 기획 및 UI/UX Wireframe 그려 원활한 개발 진행하였으며, MVC 패턴을 이용한 CRUD 활용하였습니다.</p>
-                                    <p>프로젝트 기획, 개발, 리팩토링 모두 혼자 배우고 공부하며 구현한 프로젝트 입니다.</p>
+                                    <p>전체적인 <span>웹 페이지 기획 및 UI/UX Wireframe 그려 원활한 개발 진행하였으며, MVC 패턴을 이용한 CRUD 활용</span>하였습니다.</p>
+                                    <p><span>프로젝트 기획, 개발, 리팩토링 모두 혼자 배우고 공부하며 구현한 프로젝트</span> 입니다.</p>
                                 </div>
                                 <img className="closeIcon" src={closeIcon} alt="" onClick={()=>{setGhibliModal(false)}}/>
                             </ModalWrapper>
