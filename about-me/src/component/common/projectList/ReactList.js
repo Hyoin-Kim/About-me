0064803643cdd5dade82dc1d9bfed559d0f345de
Hyoin-kim <hi980506@naver.com>
2021-11-05 14:05:01 +0900

    refactor : 스타일 수정 및 불필요한 코드 정리

--- about-me/src/component/common/projectList/ReactList.js
+++ about-me/src/component/common/projectList/ReactList.js
@@ -15,10 +15,9 @@ const ProjectWrapper = styled.div`
         &__main{
             font-family: 'Noto Sans KR', sans-serif;
             display :flex;
-            margin : 1px;
+            margin : 10px;
             justify-content : center;
             cursor : pointer;
-            border: 1px solid;
             width : 300px;
             height : 300px;
 
@@ -26,6 +25,7 @@ const ProjectWrapper = styled.div`
         &__img{
             transition: all 0.5s;
             opacity : 0.5;
+            border-radius : 20px;
         }
         &__img:hover{
             transition: all 0.5s;
@@ -48,16 +48,14 @@ const ModalWrapper = styled.div`
         border-width: 1px;
         border-style: solid;
         border-radius: 16px;
-        border-color:black;
         background-color: white;
         width: 700px;
         height: 800px;
-        opacity : ${(props) => (props.isBlur ? 0.3 : undefined)};
-        background-color : ${(props) => (props.isBlur ? 'rgba(0,0,0,0.5)' : undefined)};
+        z-index :1;
 
         .closeIcon{
             width:20px;
-            float:right;
+            float :right;
             margin : 10px;
             z-index :1;
             cursor :pointer;
@@ -85,27 +83,31 @@ const ModalWrapper = styled.div`
                 font-size : 17px;
                 margin: 10px 0px 0px 5px;
             }
+            &__o2Title{
+                font-size : 20px;
+                font-weight : bold;
+            }
         }
 
         p{
             margin-left : 10px;
             margin-right : 10px;
+            text-align : start;
+        }
+        span{
+            color : #eb285d;
         }
 `;
 
 const Modal = styled.div`
-        position: fixed;
-        z-index : 100;
-          position : fixed;
-          top : 0;
-          left : 0;
-          overflow-y: scroll; 
-          width: 100%;
-
+    position: fixed;
+    top:0; left: 0; bottom: 0; right: 0;
+    background: rgba(0, 0, 0, 0.8);
+    z-index :1;
+    overflow:hidden;
 `;
 
-const ReactProject = (props) => {
-    const { isBlur } = props;
+const ReactProject = () => {
     const [o2Modal,setO2Modal] = useState(false);
     const [sopkathonModal, setSopkathonModal] = useState(false);
     const [aboutmeModal,setAboutmeModal] = useState(false);
@@ -147,8 +149,8 @@ const ReactProject = (props) => {
                     {
                         o2Modal===true
                         ?
-                        <Modal isBlur={props.isBlur}>
-                            <ModalWrapper isBlur={props.isBlur}>
+                        <Modal>
+                            <ModalWrapper>
                                 <Carousel>
                                     <Carousel.Item variant="dark">
                                     <img className="sop__slider"src={o2SliderImg} alt=""/>
@@ -157,13 +159,13 @@ const ReactProject = (props) => {
                                     <img className="sop__slider"src={o2SliderImg2} alt=""/>
                                     </Carousel.Item>
                                     <Carousel.Item variant="dark">
+                                    <img className="sop__slider"src={o2SliderImg5} alt=""/>
+                                    </Carousel.Item>
+                                    <Carousel.Item variant="dark">
                                     <img className="sop__slider"src={o2SliderImg3} alt=""/>
                                     </Carousel.Item>
                                     <Carousel.Item variant="dark">
                                     <img className="sop__slider"src={o2SliderImg4} alt=""/>
-                                    </Carousel.Item>
-                                    <Carousel.Item variant="dark">
-                                    <img className="sop__slider"src={o2SliderImg5} alt=""/>
                                     </Carousel.Item>
                                     <Carousel.Item variant="dark">
                                     <img className="sop__slider"src={o2SliderImg6} alt=""/>
@@ -175,15 +177,16 @@ const ReactProject = (props) => {
                                     <img className="sop__slider"src={o2SliderImg8} alt=""/>
                                     </Carousel.Item>
                                 </Carousel>
-                                <div>
+                                <div className="text">
                                     <h2 className="modal__title">창업가들을 위한 회고 커뮤니티 사이트, Open Together</h2>
                                     <h5 className="modal__subtitle">타인과의 정보를 공유하여 성장하고 마침내 성공적인 결과를 만들어 낼 수 있는 가치 추구</h5>
                                     <hr className="modal__hr"/>
-                                    <p>창업가들, 예비창업가들의 앞으로 펼쳐질 창업을 위한 회고 커뮤니티 사이트 입니다.</p>
+                                    <p>창업가들, 예비창업가들의 앞으로 펼쳐질 창업을 위한 <span className="modal__o2Title">회고 커뮤니티 사이트</span>입니다.</p>
                                     <p>LearnMyself를 통해 회고 이야기를 공유할 수 있고, ShareTogether을 통해 창업가들의 성공 스토리를 공유할 수 있습니다.</p>
-                                    <p>기획,디자인,서버,웹파트원과 협업한 장기 프로젝트이며, 지금도 꾸준히 개발, QA, 운영하고 있습니다.</p>
-                                    <p>React, Typescript를 활용하여 개발하였으며, 리팩토링과 함께 Atomic Design와 NextJS 적용하였습니다.</p>
-                                    <p>메인페이지(#1), LearnMyself(#2), ShareTogether(#3), 마이페이지, 관리자 페이지 등 약 20개의 뷰 구현하였으며 최우수상 수상하였습니다.</p>
+                                    <p><span>기획,디자인,서버,웹파트원과 협업한 장기 프로젝트</span>이며, 지금도 꾸준히 개발, QA, 운영하고 있습니다.</p>
+                                    <p><span>React, Typescript</span>로 개발하였으며, 리팩토링과 함께 <span>Atomic Design와 NextJS </span>적용하였습니다.</p>
+                                    <p>메인페이지(#1), LearnMyself(#2), ShareTogether(#3), 마이페이지(#4), 관리자 페이지(#7, #8) 등 약 20개의 뷰 구현하였으며  
+                                        <span className="modal__o2Title"> 최우수상</span>을 수상하였습니다.</p>
                                 </div>
                                 <img className="closeIcon" src={closeIcon} alt="" onClick={()=>{setO2Modal(false)}}/>
                             </ModalWrapper>
@@ -207,14 +210,14 @@ const ReactProject = (props) => {
                                 <img className="sop__slider"src={sopkathonSlider3} alt=""/>
                                 </Carousel.Item>
                                 </Carousel>
-                                <div>
+                                <div className="text">
                                     <h2 className="modal__title">나만의 일기 작성하는, 궁금붕어</h2>
                                     <h5 className="modal__subtitle">오늘 뭐했붕어? 삼...이...일..... 땡!!! 오늘 있었던 일 3초 안에 대답 못하면 기억 조작!</h5>
                                     <hr className="modal__hr"/>
-                                    <p>오늘 하루에 대한 질문이 날아오면 3초안에 대답해야하는 나만의 일기 웹서비스 입니다.</p>
+                                    <p>오늘 하루에 대한 질문이 날아오면 3초안에 대답해야하는 <span className="modal__o2Title">나만의 일기 웹서비스</span> 입니다.</p>
                                     <p>게임 시작과 함게 질문, 3초안에 대답을 못할시 랜덤 답변이 주어지며, 결과 이미지에 대한 저장 및 그룹 공유가 가능합니다.</p>
-                                    <p>기획,디자인,서버,웹파트원과 협업한 단기 해커톤 프로젝트이며, 대상 수상하였습니다.</p>
-                                    <p>React, Javascript를 활용하여 개발하였으며, 개발 뿐만아니라 기획 일부에 참여하였습니다.</p>
+                                    <p><span>기획,디자인,서버,웹파트원과 협업한 무박2일 해커톤 프로젝트</span>이며, <span> '기억'을 주제로' 아이데이션 뿐만 아니라 기획 일부</span>에도 참여하였습니다.</p>
+                                    <p><span>React, Javascript를 활용</span>하여 개발하였으며, 끊임없는 소통과 협업을 통해 <span className="modal__o2Title">대상</span>을 수상하였습니다.</p>
                                 </div>
                                 <img className="closeIcon" src={closeIcon} alt="" onClick={()=>{setSopkathonModal(false)}}/>
                             </ModalWrapper>
@@ -232,13 +235,14 @@ const ReactProject = (props) => {
                                 <img className="sop__slider"src={aboutmeSlider} alt=""/>
                                 </Carousel.Item>
                                 </Carousel>
-                                <div>
+                                <div className="text">
                                     <h2 className="modal__title">나만의 포트폴리오, About Me</h2>
-                                    <h5 className="modal__subtitle">포트폴리오 용도로 제작한 React/Js 기반의 웹사이트</h5>
+                                    <h5 className="modal__subtitle">간단한 자기소개, 인적 사항, 보유 기술 스택, 프로젝트/협업 경험, GitHub/velog 링크 등</h5>
                                     <hr className="modal__hr"/>
-                                    <p>프론트엔드 공부와 더불어 배포도 경험해볼 수 있었던 프로젝트입니다.</p>
+                                    <p>포트폴리오 용도로 제작한 <span className="modal__o2Title">React/JS 기반의 웹 사이트</span>입니다.</p>
                                     <p>지금 보고 있는 웹 사이트에 해당합니다.</p>
-                                    <p>React, Javascript, bootstrap를 활용하여 개발하였습니다.</p>
+                                    <p>프론트엔드 공부와 더불어 배포 경험도 해볼 수 있었던 프로젝트 입니다. </p>
+                                    <p><span>React, Javascript, bootstrap를 활용</span>하여 개발하였습니다.</p>
                                     <p></p>
                                     <p></p>
                                 </div>
@@ -272,11 +276,12 @@ const ReactProject = (props) => {
                                 </Carousel>
                                 <div>
                                     <h2 className="modal__title">개인 프로젝트 공부를 위한 Toy Project</h2>
-                                    <h5 className="modal__subtitle">Diary App, Shopping mall, Github profile finder 등 진행 </h5>
+                                    <h5 className="modal__subtitle">Diary App, Shopping mall, Github profile finder, Clone Coding 등 진행 </h5>
                                     <hr className="modal__hr"/>
-                                    <p>IT 연합 창업동아리 SOPT 세미나, React 스터디에서 진행한 프로젝트 리스트 입니다.</p>
-                                    <p>다이어리 앱(#1), Ghibli 굿즈 쇼핑몰(#2), Github profile finder(#3), Folin Clone Coding(#4)</p>
-                                    <p>React, Javascript를 활용하여 개발하였으며, 프론트엔드의 전반적인 공부를 하는데 좋은 경험이 되었습니다.</p>
+                                    <p>IT 연합 창업동아리 SOPT 세미나, React 스터디에서 진행한 <span className="modal__o2Title"> 웹 사이트 리스트</span> 입니다.</p>
+                                    <p>다이어리 앱(#1), Ghibli 굿즈 쇼핑몰(#2), Github profile finder(#3), Folin Clone Coding(#4) 등 꾸준히 토이 프로젝트를 진행하였습니다.</p>
+                                    <p>React 공부를 중점적으로 하여 스터디와 세미나를 통해 코드리뷰 등 함께 공부하고 공유할수 있었습니다.</p>
+                                    <p><span>React, Javascript를 활용하여 개발하였으며, 프론트엔드의 전반적인 공부를 하는데 좋은 경험</span>이 되었습니다.</p>
                                 </div>
                                 <img className="closeIcon" src={closeIcon} alt="" onClick={()=>{setToyprojectModal(false)}}/>
                             </ModalWrapper>
